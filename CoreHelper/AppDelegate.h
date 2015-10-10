91342d1e94d3732e540e3d87a384542c6bbfff54
chengbin <chengbin@globalroam.com>
2015-10-10 14:58:56 +0800

    Initial Commit

---
+++ CoreHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreHelper
+//
+//  Created by chengbin on 15/10/10.
+//  Copyright © 2015年 chengbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
